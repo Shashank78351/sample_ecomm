faffb4bc6de046f8f331af6e46a263b9adbe7c9b
root <admin@example.com>
2024-06-26 14:29:42 +0530

    imported components

--- e-Commerce-main/src/app/app-routing.module.ts
+++ e-Commerce-main/src/app/app-routing.module.ts
@@ -1,26 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-
 import { SaleComponent } from './pages/sale/sale.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ConfirmOrderComponent } from './confirm-order/confirm-order.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent
-  },
-  {
-    path: 'products',
-    component: ProductListComponent
-  }, 
-  
-  {
-    path: 'sale',
-    component: SaleComponent
-  },
+ 
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path: 'products',component: ProductListComponent}, 
+  {path: 'sale',component: SaleComponent},
   { path:'confirm-order',component:ConfirmOrderComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
--- e-Commerce-main/src/app/app.module.ts
+++ e-Commerce-main/src/app/app.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { SaleComponent } from './pages/sale/sale.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ConfirmOrderComponent } from './confirm-order/confirm-order.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     SaleComponent,
     ProductListComponent,
-    ConfirmOrderComponent
+    ConfirmOrderComponent,
+    SignupComponent,
+    LoginComponent
     
   ],
   imports: [
