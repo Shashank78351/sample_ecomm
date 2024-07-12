ec4d345d83e6f755f6864df37d5f0a2d27f08ae4
Administrator <admin@example.com>
2024-07-12 07:29:36 +0000

    Update file auth.service.ts

--- e-Commerce-main/src/app/services/auth.service.ts
+++ e-Commerce-main/src/app/services/auth.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
- 
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
-private apiUrl = 'http://localhost:3002/';
- 
+  private apiUrl = 'http://e-commerceservice-backend:3002/';
+
   constructor(private http: HttpClient) { }
- 
+
   signup(username: string, email: string, password: string): Observable<any> {
-return this.http.post(`http://localhost:3002/signup`, { username, email, password });
+    return this.http.post(`${this.apiUrl}/signup`, { username, email, password });
   }
- 
-  login(username: string,email:string, password: string): Observable<any> {
-return this.http.post(`http://localhost:3002/login`, { username,email, password });
+
+  login(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { username, email, password });
   }
 }
