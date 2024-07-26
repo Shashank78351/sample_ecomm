64b77bb865d0bc5939509dce28025adb7e6c3433
Administrator <admin@example.com>
2024-07-26 08:50:03 +0000

    update svc in prd svc

--- e-Commerce-main/src/app/services/product.service.ts
+++ e-Commerce-main/src/app/services/product.service.ts
@@ -7,22 +7,22 @@ import { Product } from '../product-list/product.model';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:3002/products';
+  private apiUrl = 'http://backend-svc/products';
   public cartAddedSubject = new BehaviorSubject<Product[]>([]);
   cartItems$=this.cartAddedSubject.asObservable();
   
   constructor(private http: HttpClient) { }
 
   getProduct(productId: number): Observable<Product> {
-    return this.http.get<Product>(`http://localhost:3002/productsById/${productId}`);
+    return this.http.get<Product>(`http://backend-svc/productsById/${productId}`);
   }
   
   addToCart(productId: number) : Observable<any>  {
     console.log("Added to the cart",productId);
-    return this.http.get<any>(`http://localhost:3002/addToCart/${productId} `);
+    return this.http.get<any>(`http://backend-svc/addToCart/${productId} `);
   }
   fetchCartItems():void{
-    this.http.get<Product[]>(`http://localhost:3002/fetchCartItems`).subscribe((data)=>{
+    this.http.get<Product[]>(`http://backend-svc/fetchCartItems`).subscribe((data)=>{
       this.cartAddedSubject.next(data);
     },
     (error)=>{
@@ -32,14 +32,14 @@ export class ProductService {
   );
   }
   removeFromCart(cartId: number): Observable<any> {
-    return this.http.delete<any>(`http://localhost:3002/removeFromCart/${cartId}`);
+    return this.http.delete<any>(`http://backend-svc/removeFromCart/${cartId}`);
   }
   confirmOrder(orderData:{username:string,email:string}):Observable<any> {
-    return this.http.post(`http://localhost:3002/confirmOrder`, orderData);
+    return this.http.post(`http://backend-svc/confirmOrder`, orderData);
   }
   getUserOrders(email: string): Observable<any> {
     let params = new HttpParams().set('email', email);
-    return this.http.get(`http://localhost:3002/orders`, { params });
+    return this.http.get(`http://backend-svc/orders`, { params });
   }    
   clearCart(): void {
     this.cartAddedSubject.next([]);
