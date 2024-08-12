29b1cf8924ab58900541c68e39fb8ed26021c25b
Administrator <admin@example.com>
2024-08-12 07:45:55 +0000

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,3 @@
-
 const maxRetries = 5;
 let retryCount = 0;
 
@@ -13,13 +12,17 @@ var connection = mysql.createConnection({
 
 connection.connect((err) => {
 
-  if (err) {
-
-    console.error('Error connecting to MySQL: ', err);
-
-    return;
-
-  }
+if (err) {
+            console.error('Error connecting to MySQL:', err);
+            if (retryCount < maxRetries && err.code === 'EAI_AGAIN') {
+                retryCount++;
+                console.log(`Retrying connection (${retryCount}/${maxRetries})...`);
+                setTimeout(connectToDatabase, 2000);  // Retry after 2 seconds
+            } else {
+                console.error('Max retries reached. Could not connect to MySQL.');
+            }
+            return;
+        }
 
   console.log('Connected to MySQL database');
 
