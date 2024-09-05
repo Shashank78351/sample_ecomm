7f99dc029824624f1f02fc6e5adcaa490f29461e
Administrator <admin@example.com>
2024-09-05 07:44:22 +0000

    Update file V1.1__insert_info_to _tables.sql

---
+++ sql/migrations/V1.1__insert_info_to _tables.sql
@@ -0,0 +1,29 @@
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Mobiles','abc','https://www.sathya.store/img/category/0g4mKAv6EDEpxdUt.webp',21000, 'Electronics');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Camera', 'abc','https://t3.ftcdn.net/jpg/00/79/36/04/360_F_79360425_13tH0FGR7nYTNlXWKOWtLmzk7BAikO1b.jpg',23000, 'Electronics');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Headphones', 'abc','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4GnI_aR4lo1c1o1rlX4AjoZCTVIojl-pLKw&s',2090, 'Electronics');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Laptop', 'abc','https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/PDP-Highlight-Consumer-Laptop-5-Platinum-001-1:VP1-539x440',55600, 'Electronics');
+ 
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Shirts', 'abc','https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQZcSD1Vv0HYCJWOLR7rsXMQ-gswn-UT0jT71m5O6Fr_C5g_1IYYc7HeaqgFeenN88lvuaYRRPUS3rJ9VQirAMup5QmhjNeCOHLtF0MlS738tGDcBfKZT_X',599,'Mens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Jackets', 'abc','https://m.media-amazon.com/images/I/6108ecxLVwL._AC_UY1100_.jpg',2399 ,'Mens');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Shoes', 'abc','https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/63c77c04dc6448548ccbae880189e107_9366/Galaxy_6_Shoes_Black_GW3848_01_standard.jpg',1500, 'Mens');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Wallets', 'abc','https://m.media-amazon.com/images/I/71EtdOSfqFL._AC_UY1100_.jpg',800, 'Mens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Laptop bag', 'abc','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKCu11PRxMgWxNhO57Zv30zqabny_KfvnQMw&s',2000, 'Mens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Ethnic wear', 'Traditional Men Ethnic Wear','https://5.imimg.com/data5/YE/TF/BZ/SELLER-22535983/men-s-ethic-wear-1000x1000.jpeg',1500,'Mens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Ethnic wear','The Ethnic World Related Brands', 'https://file.theethnicworld.com/image/669/Blue-And-White-Colour-Wedding-Wear-Mens-Exclusive-Wholesale-Indo-Western-Catalog-1791.jpg',2800 ,'Mens');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Bracelets', 'LUCKY CHOICE 92.5 BIS Hallmark Sterling Pure Silver Bracelet for Men & Boys','https://m.media-amazon.com/images/I/71I3E03Q7dL._SY575_.jpg',5999, 'Mens');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Bracelets', 'Pure Silver Mens Bangle Bracelet','https://karizmajewels.in/cdn/shop/files/s-l1600_6c3fcad4-25df-440c-a970-99ee17769eea.jpg?v=1690444115&width=493',6150, 'Mens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Shirts', 'The Best Men Button-Down Shirts for Summer','https://www.mensjournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTk3MjA2MTc1Mzk2MTQ0NDQ3/buttondownshirtsmain1.webp',800, 'Mens');
+ 
+ 
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Jackets', 'LEATHER RETAIL Solid Full Sleeve Biker Jacket For Women (Brown, XS)','https://assets.ajio.com/medias/sys_master/root/20230620/9tWD/64921d17d55b7d0c6380ecae/-473Wx593H-463312106-brown-MODEL.jpg',2699,'Womens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Kurta', 'WOMEN TOUCH Floral Print Straight Kurta For Women (Blue, XL) ','https://assets.ajio.com/medias/sys_master/root/20230602/o8jq/6479505642f9e729d7103610/-473Wx593H-461924914-blue-MODEL.jpg 
+',1799 ,'Womens');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Kurtaset', 'CaniBani Women Floral Print Anarkali Kurta Set For Women (Pink, XL) ','https://assets.ajio.com/medias/sys_master/root/20240311/REPf/65ef1b0b16fd2c6e6a4eb7b9/-473Wx593H-467153648-pink-MODEL.jpg',1530, 'Womens');
+insert into products ( productName, productDescription, productImage, productPrice, category) values('Jeans', 'Urbano Fashion Regular Women Dark Blue Jeans','https://rukminim2.flixcart.com/image/850/1000/xif0q/jean/g/3/n/32-womjeandm-010b-lblue-urbano-fashion-original-imagz2jusrtgsxmy.jpeg?q=90&crop=false',949, 'Womens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Bracelets', 'Sterling Silver Link Bracelet','https://assets.ajio.com/medias/sys_master/root/20230207/FFua/63e1f5d5aeb269c651147d16/-473Wx593H-6006298220-multi-MODEL.jpg',2897, 'Womens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Bracelets', 'TARAASH 925 Sterling Floral Leaf Bracelet |Pure Silver Bracelet For Women | Sterling Silver Bracelet','https://m.media-amazon.com/images/I/41-Vkve8u4L._AC_UY1100_.jpg',2051,'Womens');
+insert into products ( productName, productDescription, productImage, productPrice,category) values('Jewellery','Priyaasi Gold-Plated Temple Jewellery | Earrings in Jhumka Style | Kemp Stone Earrings for Women', 'https://m.media-amazon.com/images/I/61wJGfEI8OL._AC_UY1100_.jpg
+',700 ,'Womens');
+
+insert into cart(name,price) values("watch",5000);
+
